b6d4bbb207af5d036d4da4d58762deaea6693d93
Barotov Mustafo <barotov2075@gmail.com>
2023-12-19 15:26:12 +0500

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+
+Person person = new Person();
+
+person.FirstName = "Mustafo";
+
+person.LastName = "Barotov";
+
+person.AGE = 16;
+
+Console.Write("FirstName: ");
+Console.WriteLine(person.GetInfo());
+Console.Write("Lastname: ");
+Console.WriteLine(person.GetInfo2());
+Console.Write("AGE: ");
+Console.WriteLine(person.GetInfo3());
+public class Person
+{
+    public string FirstName;
+    public string LastName;
+
+    public int AGE;
+    public string GetInfo()
+
+    {
+        return $"{FirstName} ";
+    }
+    public string GetInfo2()
+    {
+        return $"{LastName}";
+    }
+    public string GetInfo3()
+    {
+        return $"{AGE}";
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Task2/Program.cs
@@ -0,0 +1,46 @@
+﻿
+Person person = new Person();
+
+person.Text = "Laykro faromush nakuned";
+
+person.Likes = 463;
+
+person.Commets = 32;
+
+Console.Write("Text: ");
+Console.WriteLine(person.GetInfo());
+Console.Write("Likes: ");
+Console.WriteLine(person.GetInfo2());
+Console.Write("Comments: ");
+Console.WriteLine(person.GetInfo3());
+class Person
+{
+    public string Text;
+    public int Likes;
+
+    public int Commets;
+    public string GetInfo()
+
+    {
+        return $"{Text} ";
+    }
+    public string GetInfo2()
+    {
+        return $"{Likes}";
+    }
+    public string GetInfo3()
+    {
+        return $"{Commets}";
+    }
+
+
+
+
+
+
+
+}
+
+
+
+
